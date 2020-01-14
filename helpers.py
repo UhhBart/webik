0f0a3fd15e857019361a102ecc9a8e24ed5e3606
Martijn <martijnschuurhuis@gmail.com>
2020-01-14 19:16:22 +0000

    adding helpers.py

---
+++ helpers.py
@@ -0,0 +1,11 @@
+def youtube_api():
+
+    # de link van nummer uit de database pakken
+
+    # alleen de belangrijke shit van de link pakken (alles achter de /wathc?v=)
+
+    # deze code doorsturen naar database
+    return None
+
+
+    #https://www.youtube.com/watch?v=4fndeDfaWCg     <-- check deze banger
